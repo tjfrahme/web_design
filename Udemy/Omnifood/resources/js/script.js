52740bfaba983c1ef34c52d7803ff533206d5b20
Tim Frahme <tjfrahme@gmail.com>
2018-07-04 15:20:57 -0700

    latest

--- Udemy/Omnifood/resources/js/script.js
+++ Udemy/Omnifood/resources/js/script.js
@@ -66,5 +66,24 @@ $(document).ready(function() {
         offset: '50%'
     });
 
+    /* Mobile Nav */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+    
+        if (icon.hasClass('ion-navicon-round')) {                   /*swaps mobile nav icon classes */
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+
+
 
 });
